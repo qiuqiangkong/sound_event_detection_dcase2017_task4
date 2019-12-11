931706a42121d1ad109ebf57f6257cd3bb5c19c8
qiuqiangkong <qiuqiangkong@gmail.com>
2019-12-11 14:10:47 +0000

    update

--- pytorch/models.py
+++ pytorch/models.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import math
+import numpy as np
 
 import torch
 import torch.nn as nn
@@ -133,9 +134,9 @@ class AttBlock(nn.Module):
         
     def forward(self, x):
         # x: (n_samples, n_in, n_time)
-        # import crash
-        # asdf
-        att = torch.exp(self.att(x) / self.temperature) + 1e-6
+        tmp = self.att(x)
+        tmp = torch.clamp(tmp, -10, 10)
+        att = torch.exp(tmp / self.temperature) + 1e-6
         norm_att = att / torch.sum(att, dim=2)[:, :, None]
         cla = self.nonlinear_transform(self.cla(x))
         x = torch.sum(norm_att * cla, dim=2)
@@ -580,12 +581,95 @@ class Cnn_9layers_Gru_FrameAtt(nn.Module):
         return output_dict
 
 
-"""
-class Cnn_9layers_GAP(nn.Module):
+####################################
+# The following Transformer modules are modified from Yu-Hsiang Huang's code:
+# https://github.com/jadore801120/attention-is-all-you-need-pytorch
+class ScaledDotProductAttention(nn.Module):
+    """Scaled Dot-Product Attention"""
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+        self.softmax = nn.Softmax(dim=2)
+
+    def forward(self, q, k, v, mask=None):
+
+        attn = torch.bmm(q, k.transpose(1, 2))
+        attn = attn / self.temperature
+
+        if mask is not None:
+            attn = attn.masked_fill(mask, -np.inf)
+
+        attn = self.softmax(attn)
+        attn = self.dropout(attn)
+        output = torch.bmm(attn, v)
+
+        return output, attn
+
+
+class MultiHead(nn.Module):
+    """Multi-Head Attention module."""
+
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
+        super().__init__()
+
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.w_qs = nn.Linear(d_model, n_head * d_k)
+        self.w_ks = nn.Linear(d_model, n_head * d_k)
+        self.w_vs = nn.Linear(d_model, n_head * d_v)
+        nn.init.normal_(self.w_qs.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
+        nn.init.normal_(self.w_ks.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
+        nn.init.normal_(self.w_vs.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_v)))
+        self.w_qs.bias.data.fill_(0)
+        self.w_ks.bias.data.fill_(0)
+        self.w_vs.bias.data.fill_(0)
+
+        self.attention = ScaledDotProductAttention(temperature=np.power(d_k, 0.5))
+        self.layer_norm = nn.LayerNorm(d_model)
+
+        self.fc = nn.Linear(n_head * d_v, d_model)
+        nn.init.xavier_normal_(self.fc.weight)
+        self.fc.bias.data.fill_(0)
+
+        self.dropout = nn.Dropout(dropout)
+
+
+    def forward(self, q, k, v, mask=None):
+
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+
+        sz_b, len_q, _ = q.size()   # (batch_size, 80, 512)
+        sz_b, len_k, _ = k.size()
+        sz_b, len_v, _ = v.size()
+
+        residual = q
+
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k) # (batch_size, T, 8, 64)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        q = q.permute(2, 0, 1, 3).contiguous().view(-1, len_q, d_k) # (n*b) x lq x dk, (batch_size*8, T, 64)
+        k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k) # (n*b) x lk x dk
+        v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v) # (n*b) x lv x dv
+
+        # mask = mask.repeat(n_head, 1, 1) # (n*b) x .. x ..
+        output, attn = self.attention(q, k, v, mask=mask)   # (n_head * batch_size, T, 64), (n_head * batch_size, T, T)
+        
+        output = output.view(n_head, sz_b, len_q, d_v)  # (n_head, batch_size, T, 64)
+        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1) # b x lq x (n*dv), (batch_size, T, 512)
+        output = F.relu_(self.dropout(self.fc(output)))
+        return output
+
+
+class Cnn_9layers_Transformer_FrameAvg(nn.Module):
     def __init__(self, sample_rate, window_size, hop_size, mel_bins, fmin, 
         fmax, classes_num):
         
-        super(Cnn_9layers_GAP, self).__init__()
+        super(Cnn_9layers_Transformer_FrameAvg, self).__init__()
 
         window = 'hann'
         center = True
@@ -615,6 +699,13 @@ class Cnn_9layers_GAP(nn.Module):
         self.conv_block3 = ConvBlock(in_channels=128, out_channels=256)
         self.conv_block4 = ConvBlock(in_channels=256, out_channels=512)
 
+        n_head = 8
+        n_hid = 512
+        d_k = 64
+        d_v = 64
+        dropout = 0.2
+        self.multihead = MultiHead(n_head, n_hid, d_k, d_v, dropout)
+
         self.fc = nn.Linear(512, classes_num, bias=True)
 
         self.init_weights()
@@ -624,7 +715,7 @@ class Cnn_9layers_GAP(nn.Module):
         init_layer(self.fc)
 
     def forward(self, input, mixup_lambda=None):
-        '''Input: (batch_size, times_steps, freq_bins)'''
+        """Input: (batch_size, times_steps, freq_bins)"""
         
         interpolate_ratio = 8
 
@@ -641,13 +732,16 @@ class Cnn_9layers_GAP(nn.Module):
         # Mixup on spectrogram
         if self.training and mixup_lambda is not None:
             x = do_mixup(x, mixup_lambda)
-      
+        
         x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
         x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
         x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
         x = self.conv_block4(x, pool_size=(1, 1), pool_type='avg')
-        
+
         x = torch.mean(x, dim=3)
+        x = x.transpose(1, 2)   # (batch_size, time_steps, channels)
+        x = self.multihead(x, x, x)
+        x = x.transpose(1, 2)
         embedding = x   # (batch_size, feature_maps, time_steps)
 
         # Framewise output
@@ -656,8 +750,7 @@ class Cnn_9layers_GAP(nn.Module):
         framewise_output = interpolate(framewise_output, interpolate_ratio)
         
         # Clipwise output
-        x = torch.mean(embedding, dim=2)
-        clipwise_output = torch.sigmoid(self.fc(x))
+        clipwise_output = torch.mean(framewise_output, dim=1)
         
         output_dict = {
             'framewise_output': framewise_output, 
@@ -667,11 +760,11 @@ class Cnn_9layers_GAP(nn.Module):
         return output_dict
 
 
-class Cnn_9layers_GMP(nn.Module):
+class Cnn_9layers_Transformer_FrameAtt(nn.Module):
     def __init__(self, sample_rate, window_size, hop_size, mel_bins, fmin, 
         fmax, classes_num):
         
-        super(Cnn_9layers_GMP, self).__init__()
+        super(Cnn_9layers_Transformer_FrameAtt, self).__init__()
 
         window = 'hann'
         center = True
@@ -701,16 +794,22 @@ class Cnn_9layers_GMP(nn.Module):
         self.conv_block3 = ConvBlock(in_channels=128, out_channels=256)
         self.conv_block4 = ConvBlock(in_channels=256, out_channels=512)
 
-        self.fc = nn.Linear(512, classes_num, bias=True)
+        n_head = 8
+        n_hid = 512
+        d_k = 64
+        d_v = 64
+        dropout = 0.2
+        self.multihead = MultiHead(n_head, n_hid, d_k, d_v, dropout)
+
+        self.att_block = AttBlock(n_in=512, n_out=17, activation='sigmoid')
 
         self.init_weights()
 
     def init_weights(self):
         init_bn(self.bn0)
-        init_layer(self.fc)
 
     def forward(self, input, mixup_lambda=None):
-        '''Input: (batch_size, times_steps, freq_bins)'''
+        """Input: (batch_size, times_steps, freq_bins)"""
         
         interpolate_ratio = 8
 
@@ -727,115 +826,24 @@ class Cnn_9layers_GMP(nn.Module):
         # Mixup on spectrogram
         if self.training and mixup_lambda is not None:
             x = do_mixup(x, mixup_lambda)
-      
+        
         x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
         x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
         x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
         x = self.conv_block4(x, pool_size=(1, 1), pool_type='avg')
-        
-        x = torch.mean(x, dim=3)
-        embedding = x   # (batch_size, feature_maps, time_steps)
 
-        # Framewise output
+        x = torch.mean(x, dim=3)
+        x = x.transpose(1, 2)   # (batch_size, time_steps, channels)
+        x = self.multihead(x, x, x)
         x = x.transpose(1, 2)
-        framewise_output = torch.sigmoid(self.fc(x))
-        framewise_output = interpolate(framewise_output, interpolate_ratio)
-        
-        # Clipwise output
-        (x, _) = torch.max(embedding, dim=2)
-        clipwise_output = torch.sigmoid(self.fc(x))
-        
-        output_dict = {
-            'framewise_output': framewise_output, 
-            'clipwise_output': clipwise_output, 
-            'embedding': embedding}
-            
-        return output_dict
-
-
-class Cnn_9layers_ATT(nn.Module):
-    def __init__(self, sample_rate, window_size, hop_size, mel_bins, fmin, 
-        fmax, classes_num):
-        
-        super(Cnn_9layers_ATT, self).__init__()
-
-        window = 'hann'
-        center = True
-        pad_mode = 'reflect'
-        ref = 1.0
-        amin = 1e-10
-        top_db = None
-
-        # Spectrogram extractor
-        self.spectrogram_extractor = Spectrogram(n_fft=window_size, hop_length=hop_size, 
-            win_length=window_size, window=window, center=center, pad_mode=pad_mode, 
-            freeze_parameters=True)
-
-        # Logmel feature extractor
-        self.logmel_extractor = LogmelFilterBank(sr=sample_rate, n_fft=window_size, 
-            n_mels=mel_bins, fmin=fmin, fmax=fmax, ref=ref, amin=amin, top_db=top_db, 
-            freeze_parameters=True)
-
-        # Spec augmenter
-        self.spec_augmenter = SpecAugmentation(time_drop_width=64, time_stripes_num=2, 
-            freq_drop_width=8, freq_stripes_num=2)
-
-        self.bn0 = nn.BatchNorm2d(64)
-
-        self.conv_block1 = ConvBlock(in_channels=1, out_channels=64)
-        self.conv_block2 = ConvBlock(in_channels=64, out_channels=128)
-        self.conv_block3 = ConvBlock(in_channels=128, out_channels=256)
-        self.conv_block4 = ConvBlock(in_channels=256, out_channels=512)
-        self.att_block = AttBlock(n_in=512, n_out=512, activation='linear')
-
-        self.fc = nn.Linear(512, classes_num, bias=True)
-
-        self.init_weights()
-
-    def init_weights(self):
-        init_bn(self.bn0)
-        init_layer(self.fc)
-
-    def forward(self, input, mixup_lambda=None):
-        '''Input: (batch_size, times_steps, freq_bins)'''
-        
-        interpolate_ratio = 8
-
-        x = self.spectrogram_extractor(input)   # (batch_size, 1, time_steps, freq_bins)
-        x = self.logmel_extractor(x)    # (batch_size, 1, time_steps, mel_bins)
-        
-        x = x.transpose(1, 3)
-        x = self.bn0(x)
-        x = x.transpose(1, 3)
-        
-        if self.training:
-            x = self.spec_augmenter(x)
-
-        # Mixup on spectrogram
-        if self.training and mixup_lambda is not None:
-            x = do_mixup(x, mixup_lambda)
-      
-        x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
-        x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
-        x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
-        x = self.conv_block4(x, pool_size=(1, 1), pool_type='avg')
-        
-        x = torch.mean(x, dim=3)
         embedding = x   # (batch_size, feature_maps, time_steps)
 
-        (x, norm_att, cla) = self.att_block(embedding)
-        ''''cla: (batch_size, channels, time_stpes)'''
-
-        x = F.relu(x)
-        cla = F.relu(cla)
+        (clipwise_output, norm_att, cla) = self.att_block(x)
+        """cla: (batch_size, classes_num, time_stpes)"""
 
         # Framewise output
-        cla = cla.transpose(1, 2)
-        framewise_output = torch.sigmoid(self.fc(cla))
+        framewise_output = cla.transpose(1, 2)
         framewise_output = interpolate(framewise_output, interpolate_ratio)
-        
-        # Clipwise output
-        clipwise_output = torch.sigmoid(self.fc(x))
         
         output_dict = {
             'framewise_output': framewise_output, 
@@ -843,4 +851,3 @@ class Cnn_9layers_ATT(nn.Module):
             'embedding': embedding}
             
         return output_dict
-"""
